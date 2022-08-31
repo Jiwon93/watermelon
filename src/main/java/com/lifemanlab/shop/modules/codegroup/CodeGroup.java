76c722af2fc85ea33ceae201e53401f3294bfef6
Jiwon93 <ls_sjw93@naver.com>
2022-08-31 17:40:11 +0900

    m

---
+++ src/main/java/com/lifemanlab/shop/modules/codegroup/CodeGroup.java
@@ -0,0 +1,43 @@
+package com.lifemanlab.shop.modules.codegroup;
+
+public class CodeGroup {
+
+	private String ccgSeq;
+	private String ccgName;
+	private Integer ccgOrder;
+	private Integer ccgUseNY;
+	private Integer ccgDelNY;
+///////////////////////////////////
+	public String getCcgSeq() {
+		return ccgSeq;
+	}
+	public void setCcgSeq(String ccgSeq) {
+		this.ccgSeq = ccgSeq;
+	}
+	public String getCcgName() {
+		return ccgName;
+	}
+	public void setCcgName(String ccgName) {
+		this.ccgName = ccgName;
+	}
+	public Integer getCcgOrder() {
+		return ccgOrder;
+	}
+	public void setCcgOrder(Integer ccgOrder) {
+		this.ccgOrder = ccgOrder;
+	}
+	public Integer getCcgUseNY() {
+		return ccgUseNY;
+	}
+	public void setCcgUseNY(Integer ccgUseNY) {
+		this.ccgUseNY = ccgUseNY;
+	}
+	public Integer getCcgDelNY() {
+		return ccgDelNY;
+	}
+	public void setCcgDelNY(Integer ccgDelNY) {
+		this.ccgDelNY = ccgDelNY;
+	}
+		
+	
+}

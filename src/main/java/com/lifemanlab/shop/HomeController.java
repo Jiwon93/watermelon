1c9c1323a915ed47e70510f5e720c5d47f267d65
Jiwon93 <ls_sjw93@naver.com>
2022-09-12 22:48:58 +0900

    commit

--- src/main/java/com/lifemanlab/shop/HomeController.java
+++ src/main/java/com/lifemanlab/shop/HomeController.java
@@ -22,6 +22,11 @@ public class HomeController {
 		 * Simply selects the home view to render by returning its name.
 		 */
 		
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+		public String index(Locale locale, Model model) {
+			return "index";
+		}
+		
 		@RequestMapping(value = "/a", method = RequestMethod.GET)
 		public String a(Locale locale, Model model) {
 			logger.info("Welcome home! The client locale is {}.", locale);

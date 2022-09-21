54a99b4dd957d55f72e9ea7c2d9a1ad83ed0c163
Jiwon93 <ls_sjw93@naver.com>
2022-09-21 23:22:29 +0900

    commit

--- src/main/java/com/lifemanlab/shop/modules/member/MemberDao.java
+++ src/main/java/com/lifemanlab/shop/modules/member/MemberDao.java
@@ -22,10 +22,26 @@ public class MemberDao {
 		return list; 
 	}
 	
+	public int selectOneCount(MemberVo vo) {
+		int count = sqlSession.selectOne(namespace + ".selectOneCount", vo);
+		System.out.println("dao count: " + count);
+		return count;
+	}
+	
+	public Member selectOne(MemberVo vo) {
+		Member item = sqlSession.selectOne(namespace + ".selectOne", vo);
+		System.out.println("dao item: " + item);
+		return item;
+	}
+	
 	public int insert(Member dto) {
 		int result = sqlSession.insert(namespace + ".insert", dto);
 		System.out.println("dao result: " + result);
 		return result;
 	}
+	
+	public int update(Member dto) { return sqlSession.update(namespace + ".update", dto); }
+	public int uelete(Member dto) { return sqlSession.update(namespace + ".uelete", dto); }
+	public int delete(MemberVo vo) { return sqlSession.delete(namespace + ".delete", vo); }
 
 }
--- src/main/java/com/lifemanlab/shop/modules/member/MemberServiceImpl.java
+++ src/main/java/com/lifemanlab/shop/modules/member/MemberServiceImpl.java
@@ -13,16 +13,49 @@ public class MemberServiceImpl implements MemberService {
 	
 	@Override
 	public List<Member> selectList(MemberVo vo) throws Exception {
-		
 		List<Member> list = dao.selectList(vo);
 		return list;
 	}
 
 	@Override
+	public int selectOneCount(MemberVo vo) throws Exception {
+		int count = dao.selectOneCount(vo);
+		System.out.println("service count: " + count);
+		return count;
+	}
+
+	@Override
+	public Member selectOne(MemberVo vo) throws Exception {
+		Member item = dao.selectOne(vo);
+		System.out.println("service item: " + item);
+		return item;
+	}
+	
+	@Override
 	public int insert(Member dto) throws Exception {
 		int result = dao.insert(dto);
 		System.out.println("service result: " + result);
 		return result;
+	}
+
+	
+
+	@Override
+	public int update(Member dto) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int uelete(Member dto) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(MemberVo vo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	
